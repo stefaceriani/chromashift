b8465dd6cee67596cbc828a325a8ca51be4971f6
stefaceriani <stefano.ceriani@blu.it>
2026-03-18 22:38:29 +0100

    Update golden_hour.js

--- custom preset/golden_hour.js
+++ custom preset/golden_hour.js
@@ -2,4 +2,18 @@
 // REQUESTEd BY: @stefaceriani
 // CS VERSION: 2.5.3
 // PRESET VERSION: 1.0.0
-// Coming Soon...
+
+},
+    golden: {
+      name: "Golden Hour", emoji: "🌙", builtin: true,
+      colors: {
+        csText: "#fff8e7", csSubtext: "#fcd97a",
+        csMain: "#12100a", csMainElevated: "#1c1810",
+        csHighlight: "#2a2210", csHighlightElevated: "#3d3318",
+        csAccent: "#f5c518", csPlayButton: "#f5c518", csPlayButtonHover: "#ffd740",
+        csButtonDisabled: "#4a3d10", csSidebar: "#0a0806",
+        csPlayer: "#12100a", csCard: "#1e1a0e", csCardHover: "#2e2814",
+        csNotification: "#f5c518", csProgressBg: "#3d3318", csProgressFg: "#f5c518", csVolumeBg: "#3d3318", csVolumeFg: "#fcd97a",
+      },
+    },
+  }
