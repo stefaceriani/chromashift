875ca94c89cb1db138a3b16941b0f59772b63501
Chromashift-Backup <info.myworldtraveller@gmail.com>
2026-04-03 14:24:37 +0200

    Update chromashift.js

--- chromashift.js
+++ chromashift.js
@@ -116,6 +116,31 @@
         csNotification: "#5c6bc0", csProgressBg: "#253050", csProgressFg: "#5c6bc0", csVolumeBg: "#253050", csVolumeFg: "#5c6bc0",
       },
     },
+purple: {
+  name: "22 Purple",
+  emoji: "💜",
+  colors: {
+    csText: "#f5eaff",
+    csSubtext: "#d88fff",
+    csMain: "#0d0a12",
+    csMainElevated: "#150f1e",
+    csHighlight: "#1e1530",
+    csHighlightElevated: "#2a1d42",
+    csAccent: "#a742f5",
+    csPlayButton: "#a742f5",
+    csPlayButtonHover: "#c06bff",
+    csButtonDisabled: "#4a1f70",
+    csSidebar: "#080610",
+    csPlayer: "#0d0a12",
+    csCard: "#1a1228",
+    csCardHover: "#241a38",
+    csNotification: "#a742f5",
+    csProgressBg: "#2a1d42",
+    csProgressFg: "#a742f5",
+    csVolumeBg: "#2a1d42",
+    csVolumeFg: "#d88fff"
+  },
+},
     rose: {
       name: "Rose Gold", emoji: "🌸", builtin: true,
       colors: {
