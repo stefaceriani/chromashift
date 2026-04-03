ea585beefd83669979de589dd77e39c1ad3cbea5
Chromashift-Backup <info.myworldtraveller@gmail.com>
2026-04-03 14:23:20 +0200

    Create 22_purple.js

---
+++ custom_preset/22_purple.js
@@ -0,0 +1,30 @@
+// PRESET NAME: 💜 22 Purple
+// REQUESTED BY: 
+// CS VERSION: 2.5.3
+// PRESET VERSION: 1.0.0
+
+purple: {
+  name: "22 Purple",
+  emoji: "💜",
+  colors: {
+    csText: "#f5eaff",
+    csSubtext: "#d88fff",
+    csMain: "#0d0a12",
+    csMainElevated: "#150f1e",
+    csHighlight: "#1e1530",
+    csHighlightElevated: "#2a1d42",
+    csAccent: "#a742f5",
+    csPlayButton: "#a742f5",
+    csPlayButtonHover: "#c06bff",
+    csButtonDisabled: "#4a1f70",
+    csSidebar: "#080610",
+    csPlayer: "#0d0a12",
+    csCard: "#1a1228",
+    csCardHover: "#241a38",
+    csNotification: "#a742f5",
+    csProgressBg: "#2a1d42",
+    csProgressFg: "#a742f5",
+    csVolumeBg: "#2a1d42",
+    csVolumeFg: "#d88fff"
+  }
+}
